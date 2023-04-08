f6d5e481de98862d380b674c60d85d098164869b
Abir Hasan <98998279+h-abir@users.noreply.github.com>
2023-04-09 02:04:29 +0600

    Update Page1-soln.c

--- Loop/Page1-soln.c
+++ Loop/Page1-soln.c
@@ -1,3 +1,26 @@
 // 1+2+3+ .....n
 
 #include<stdio.h>
+int main(){
+    int n,s;
+    scanf("%d",&n);
+    s= (n*(n+1))/2;
+    printf("%d\n",s);
+    
+  
+    return 0;
+}
+
+// 1^2 + 2^2 + 3^2 + ..... + n^2
+
+#include<stdio.h>
+int main(){
+    int n,s;
+    scanf("%d",&n);
+    s= (n*(n+1))/2;
+    printf("%d\n",s);
+    
+  
+    return 0;
+}
+
