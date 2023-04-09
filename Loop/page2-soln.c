569fffeaf9628efaffc179a420835a490bcce391
Abir Hasan <98998279+h-abir@users.noreply.github.com>
2023-04-09 16:40:42 +0600

    Create page2-soln.c

---
+++ Loop/page2-soln.c
@@ -0,0 +1,15 @@
+// 1-2+3-4+5- .... n
+
+#include <stdio.h>
+int main(){
+  int n,s=0;
+  scanf("%d",&n);
+  for(int i=0; i<=n; i++){
+    if(i&1){
+      s=s+i;
+    }
+    else s=s-i;
+  }
+  
+  retrun 0;
+}
